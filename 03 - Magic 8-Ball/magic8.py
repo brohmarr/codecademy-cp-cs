916282beb96e77407662f195cf0bf35c5eb74055
Brohmarr <panza.thiagoj@gmail.com>
2023-01-04 23:03:59 -0300

    Add description to all scripts

--- 03 - Magic 8-Ball/magic8.py
+++ 03 - Magic 8-Ball/magic8.py
@@ -1,8 +1,12 @@
 # Project: Magic 8-Ball
 # by: @Brohmarr
 
-# The "Magic 8-Ball" is a popular toy developed in the 1950s for fortune-telling or advice seeking.
-# Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+# The "Magic 8-Ball" is a popular toy developed in the 1950s for
+#     fortune-telling or advice seeking.
+# 
+# Write a magic8.py Python program that can answer any “Yes” or “No”
+#     question with a different fortune each time it executes.
+
 
 import random
 
