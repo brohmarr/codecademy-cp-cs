ed9025143851d2e38b57ede18c0188f14f8efeac
Brohmarr <panza.thiagoj@gmail.com>
2023-01-05 22:26:43 -0300

    Code clean up

--- 02 - Receipts for Lovely Loveseats/script.py
+++ 02 - Receipts for Lovely Loveseats/script.py
@@ -15,15 +15,20 @@
 
 # Inventory #
 # Item: "Lovely Loveseat"
-lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+lovely_loveseat_description = ("Lovely Loveseat. Tufted polyester blend on "
+                               "wood. 32 inches high x 40 inches wide x 30 "
+                               "inches deep. Red or white.")
 lovely_loveseat_price = 254.00
 
 # Item: "Stylish Settee"
-stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+stylish_settee_description = ("Stylish Settee. Faux leather on birch. 29.50 "
+                             "inches high x 54.75 inches wide x 28 inches "
+                             "deep. Black.")
 stylish_settee_price = 180.50
 
 # Item: "Luxurious Lamp"
-luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+luxurious_lamp_description = ("Luxurious Lamp. Glass and iron. 36 inches tall."
+                              " Brown with cream shade.")
 luxurious_lamp_price = 52.15
 
 # Defining the sales tax value.
@@ -41,13 +46,15 @@ customer_one_total_items = 0
 customer_one_total += lovely_loveseat_price
 customer_one_total_items += 1
 customer_one_new_item_text = "(" + str(customer_one_total_items) + ") "
-customer_one_itemization += customer_one_new_item_text + lovely_loveseat_description
+customer_one_itemization += (customer_one_new_item_text +
+                             lovely_loveseat_description)
 
 # Customer One makes their second purchase.
 customer_one_total += luxurious_lamp_price
 customer_one_total_items += 1
 customer_one_new_item_text = "(" + str(customer_one_total_items) + ") "
-customer_one_itemization += " " + customer_one_new_item_text + luxurious_lamp_description
+customer_one_itemization += (" " + customer_one_new_item_text +
+                             luxurious_lamp_description)
 
 # Customer One is ready to check out.
 # Calculating the sales tax and adding to their total.
--- 05 - Gradebook/script.py
+++ 05 - Gradebook/script.py
@@ -6,12 +6,14 @@
 
 
 # Initial Data
-last_semester_gradebook = [["Politics", 80], ["Latin", 96], ["Dance", 97], ["Architecture", 65]]
+last_semester_gradebook = [["Politics", 80], ["Latin", 96], ["Dance", 97],
+                           ["Architecture", 65]]
 
 # Initializing Lists
 subjects = ["Physics", "Calculus", "Poetry", "History"]
 grades = [98, 97, 85, 88]
-gradebook = [["Physics", 98], ["Calculus", 97], ["Poetry", 85], ["History", 88]]
+gradebook = [["Physics", 98], ["Calculus", 97], ["Poetry", 85],
+             ["History", 88]]
 
 # My grade for the Computer Science class justa came in! Perfect score!
 gradebook.append(["Computer Science", 100])
@@ -19,10 +21,12 @@ gradebook.append(["Computer Science", 100])
 # Another grade came in: Visual Arts! Got a score of 93!
 gradebook.append(["Visual Arts", 93])
 
-# My instructor made a mistake grading the visual arts class. Got 5 extra points!
+# My instructor made a mistake grading the visual arts class. Got 5
+#     extra points!
 gradebook[-1][-1] += 5
 
-# Er, I don't care that much about poetry, just need to know if I passed...
+# Er, I don't care that much about poetry, just need to know if I
+#     passed...
 gradebook[2].remove(gradebook[2][-1])
 gradebook[2].append("Pass")
 
